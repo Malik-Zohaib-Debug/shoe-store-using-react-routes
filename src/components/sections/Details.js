2908fefbbb86eab636a3110aac32255b09550f46
Malik Zohaib Zahid <zohaibzahid112233@gmail.com>
2023-06-27 15:08:07 +0500

    details page added

--- src/components/sections/Details.js
+++ src/components/sections/Details.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, {useState, useContext, useEffect} from "react";
+import { GlobalContext } from "../GlobalState/GlobalContext";
+import { Link, useParams } from "react-router-dom";
+import Colors from '../sections/Colors';
+import '../css/Details.css';
 
 export default function Details(){
+    const {products} = useContext(GlobalContext);
+    const {addCart} = useContext(GlobalContext);
+    const [product, setProduct] = useState([]);
+
+    const productID = useParams();
+    
+    useEffect(()=>{
+        getProduct();
+    }, [])
+
+
+    const getProduct = () => {
+        const getProd = products.filter((product) => product._id === productID.id);
+        setProduct(getProd);
+    }
+
+
     return(
-        <div>
-            Details
-        </div>
+        <>
+                {
+                    product.map(item =>(
+                        <div className="details" key={item._id}>
+                            <img src={item.src} alt=""/>
+                            <div className="box">
+                                <div className="row">
+                                    <h2>{item.title}</h2>
+                                    <span>${item.price}</span>
+                                </div>
+                                <Colors colors={item.colors}/>
+                                <p>{item.description}</p>
+                                <p>{item.content}</p>
+                                <Link to="/cart" className="cart" onClick={() => addCart(item._id)}>
+                                    Add to cart
+                                </Link>
+                            </div>
+                        </div>
+                    ))
+                }
+            </>
     );
 }
